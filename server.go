5dfbf6655a4661a3559630fd4f4dd1907a5e1255
dibusure <dibusure@cock.li>
2022-12-23 19:00:32 +0300

    added server

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+        "net/http"
+)
+
+func main() {
+        http.Handle("/", http.FileServer(http.Dir("./")))
+        http.Handle("/authors/", http.StripPrefix("/authors/", http.FileServer(http.Dir("./sorts/author"))))
+        http.Handle("/pubhouses/", http.StripPrefix("/pubhouses/", http.FileServer(http.Dir("./sorts/pubhouse"))))
+        http.ListenAndServe(":3000", nil)
+}
